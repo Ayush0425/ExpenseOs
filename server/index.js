02b28b3a7af905658585b6ca57e4bd7cd4605870
Ayush0425 <ayushdewal2005@gmail.com>
2026-07-03 00:35:15 +0530

    Day 3: Setup MongoDB Atlas and connect backend

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 const app = require("./app");
 
-const PORT = 5000;
+dotenv.config();
+
+// Connect Database
+connectDB();
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
