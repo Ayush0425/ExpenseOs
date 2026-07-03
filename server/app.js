1858c4fcf7808de20f51bcb379c74975bde5e4ac
Ayush0425 <ayushdewal2005@gmail.com>
2026-07-04 00:12:26 +0530

    Day 4:Complete user registration backend with MongoDB and bcrypt

--- server/app.js
+++ server/app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 
+const userRoutes = require("./routes/userRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("🚀 ExpenseOS Backend Running");
